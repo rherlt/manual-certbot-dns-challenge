a7588dcbe20437f5df6086aa3a8543cdf16c8582
Rico Herlt <mail@rherlt.com>
2023-11-08 17:51:49 +0100

    added some input validation

--- manual-certbot-dns.sh
+++ manual-certbot-dns.sh
@@ -7,6 +7,16 @@ do
     esac
 done
 
+if [ -z "${email}" ]; then
+    echo "E-Mail not given. use argument -e EMAIL to specify the email for Let's Encrypt expiring notification."
+    exit 1
+fi 
+
+if [ -z "${dns}" ]; then
+    echo "DNS not given. use argument -d DNS to specify the DNS name for the certificate to issue, eg. 'my.example.com'."
+    exit 1
+fi
+
 echo "Create Certificate for DNS-Name: $dns"
 echo "E-Mail address to use for the let's encrypt account: $email"
 
